afc533dd638e89e75dd5859420e0543ae51356d6
Sagar MIshra <sagarmish1234@gmail.com>
2023-04-21 14:29:57 +0530

    Initial Commit

---
+++ centralServer/src/test/java/com/work/hotelReservation/centralServer/CentralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.hotelReservation.centralServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
